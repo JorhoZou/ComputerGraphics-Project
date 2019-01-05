ad5217a176ac7393e4a3317b3b9ad264b61fc386
JorhoZou <771991657@qq.com>
2019-01-05 14:39:33 +0800

    调整地形

--- runrun-master/assets/libs/terraingen/gameterrain.js
+++ runrun-master/assets/libs/terraingen/gameterrain.js
@@ -7,14 +7,22 @@ var GAMETERRAIN_FILTER =
 
 		context.beginPath();
 		
-
+		/*context.fillStyle = "#000";
+		context.rect( 0, 0, inCanvas.width, inCanvas.height );
+		context.fill();*/
 		
-		// 地形
+		// Apply gradient to get a shape for a potential game
 		var gradient = context.createLinearGradient( inCanvas.width, 0, 0, 0 );
 		gradient.addColorStop( 0, '#111' );   
 		gradient.addColorStop( 0.06, '#1b1b1b' ); 
-		gradient.addColorStop( 0.1, '#1b1b1b' );   
-
+		gradient.addColorStop( 0.1, '#1b1b1b' );    
+		//gradient.addColorStop( 0.3, 'rgba(0, 0, 0, 0.7)' );   
+		//gradient.addColorStop( 0.6, 'rgba(0, 0, 0, 0.3)' );
+		//gradient.addColorStop( 0.8, 'rgba(0, 0, 0, 0.1)' );
+		//gradient.addColorStop( 0.9, 'rgba(255, 255, 255, 0.1)' );
+		//gradient.addColorStop( 0.95, 'rgba(255, 255, 255, 0.1)' );
+		//gradient.addColorStop( 1, 'rgba(0, 0, 0, 0.3)' );
+		
 		context.fillStyle = gradient;
 		context.rect( 0, 0, inCanvas.width, inCanvas.height );
 		context.fill();
@@ -23,4 +31,4 @@ var GAMETERRAIN_FILTER =
 		
 		BLUR_FILTER.Apply( inCanvas, inParameters );
 	}
-};
+};        
